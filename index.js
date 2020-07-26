a686f03806656f93bd291e4ffb85d3dd6fa4ceac
D'niel <dniel@dniel.net>
2020-07-26 11:12:29 -0300

    implementando clusters

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const http = require('http');
 const server = require('./server')
+const os = require('os');
+const cluster = require('cluster');
 const port = process.env.port || 3000;
 const index = http.createServer(server);
-index.listen(port);
+
+if(cluster.isMaster) {
+    for (let c = 0; c < os.cpus().length; c++) {
+        var worker = cluster.fork();
+        console.log(worker.id);
+    }
+} 
+else {
+
+    console.log(os.cpus());
+    index.listen(port);
+
+}
--- server.js
+++ server.js
@@ -2,21 +2,16 @@ const express = require('express');
 const server = express();
 const morgan = require('morgan');
 const fs = require('fs');
-
 server.use(morgan('dev'));
 server.use(express.json());
 
-server.get('/',(req, res, next) => {
-    res.status(200).send({
-        mensagem: 'GET funcionando'
-    });
-});
 server.post('/',(req, res, next) => {
    var arquivos =  fs.readdirSync('./json');
    var existe = false;
    
    arquivos.forEach(e => {
-        if (fs.statSync('./json/' + e).birthtimeMs > (Date.now() - 600000)) {
+    //verifica se o arquivo foi criado a menos de 10 minutos
+    if (fs.statSync('./json/' + e).birthtimeMs > (Date.now() - 600000)) { 
            if(JSON.stringify(req.body) == JSON.stringify(JSON.parse(fs.readFileSync('./json/' + e)))) {
                existe = true;
                console.log('ja existe o arquivo');
@@ -24,12 +19,18 @@ server.post('/',(req, res, next) => {
     }
 });
 
-if (existe == false)
+if (existe == false) {
     fs.writeFileSync('./json/'+ Date.now().toString(),JSON.stringify(req.body));
 
-    res.status(200).send({
-        mensagem: existe ? 'ja existe' : 'gravado' 
-    });
-});
+    res.status(200). send({
+        mensagem: 'OK' 
+    })}
+else {
+    res.status(403). send({
+        mensagem: 'Forbidden' 
+    })};
+
+    }
+);
 
 module.exports = server;
